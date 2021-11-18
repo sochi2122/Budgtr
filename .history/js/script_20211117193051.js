ed76405fd37697059f0a03196287bb5ffb1ca801
sochi2122 <90932673+sochi2122@users.noreply.github.com>
2021-11-17 20:59:47 -0500

    commit

---
+++ .history/js/script_20211117193051.js
@@ -0,0 +1,21 @@
+// const budget =
+
+
+
+// let accountArr = []
+// let totalBal = accountArr + i
+// for (let i = 0; i < budget.length; i++) {
+//   accountArr.push(Number(budget.amount))
+//   totalBal += (Number(budget.amount))
+//   console.log(totalBal)
+// }
+
+
+
+const budget
+ let sum = 0;
+  for (let i=0; i<budget.length; i++) {
+    sum += budget[i];
+}
+
+console.log(sum)
